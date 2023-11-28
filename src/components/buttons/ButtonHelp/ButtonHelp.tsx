e0157d4ecea3ac1aca459b2d949f74b9d2c6f0c9
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-28 19:38:28 +0300

    feat: add button help

---
+++ src/components/buttons/ButtonHelp/ButtonHelp.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+const ButtonHelp = () => {
+  return (
+    <Button variant="contained" size="large">
+      Задать вопрос
+    </Button>
+  );
+};
+
+export default ButtonHelp;
