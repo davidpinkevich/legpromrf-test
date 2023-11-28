dc019fda49a162a3b72da2821fbd5f69049a6132
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-28 19:38:43 +0300

    feat: add navigation

---
+++ src/components/Header/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import './Navigation.scss';
+
+const Navigation = () => {
+  return (
+    <nav className="header__body-nav nav">
+      <div className="nav__link">Все компании</div>
+      <div className="nav__link">Избранные</div>
+    </nav>
+  );
+};
+
+export default Navigation;
