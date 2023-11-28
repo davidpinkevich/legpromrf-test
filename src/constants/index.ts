6c46f8d35a69814de0b670ba64a58dbba4c0f5d4
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-28 22:22:06 +0300

    feat: add companies

---
+++ src/constants/index.ts
@@ -0,0 +1,52 @@
+import { ICompanies } from '../types';
+
+export const COMPAINES: Array<ICompanies> = [
+  {
+    id: 1234,
+    name: 'Газпром',
+    start: 1989,
+    current: 450000,
+    info: 'Крупнейшая российская газовая компания с мировым влиянием',
+    img: 'https://globalenergyprize.org/ru/wp-content/uploads/2023/02/Gazprom.jpg',
+  },
+  {
+    id: 5678,
+    name: 'Ростелеком',
+    start: 1992,
+    current: 20000,
+    info: 'Ведущий российский оператор связи и интернет-провайдер',
+    img: 'https://im.kommersant.ru/Issues.photo/DAILY/2022/208/KSP_017606_00046_1_t222_203908.jpg',
+  },
+  {
+    id: 9876,
+    name: 'Лукойл',
+    start: 1991,
+    current: 150000,
+    info: 'Один из крупнейших российских нефтяных компаний с международным присутствием',
+    img: 'https://s0.rbk.ru/v6_top_pics/media/img/6/21/755879843019216.jpg',
+  },
+  {
+    id: 7890,
+    name: 'Магнит',
+    start: 1994,
+    current: 300000,
+    info: 'Сеть розничных магазинов, лидер в сфере продаж продуктов питания',
+    img: 'https://incrussia.ru/wp-content/uploads/2023/06/iStock-643342522.jpg',
+  },
+  {
+    id: 3456,
+    name: 'МегаФон',
+    start: 2002,
+    current: 25000,
+    info: 'Один из крупнейших мобильных операторов в России',
+    img: 'https://cdn.forbes.ru/forbes-static/new/2023/08/1-TASS-60657760-64e30c232b991.jpg',
+  },
+  {
+    id: 6789,
+    name: 'МТС',
+    start: 1993,
+    current: 60000,
+    info: 'Ведущий телекоммуникационный оператор, предоставляющий услуги связи и интернета',
+    img: 'https://img.gazeta.ru/files3/989/16481989/upload-RIA_8401509-pic_32ratio_1200x800-1200x800-51579.jpg',
+  },
+];
