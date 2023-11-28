e4fa9a7b6d198900906534763c73efc0c7700e48
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-28 19:37:33 +0300

    feat: add normalize.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import '../src/styles/normalize.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
